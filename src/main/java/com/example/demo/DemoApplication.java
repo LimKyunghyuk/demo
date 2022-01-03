e1437efc08d8c501658a4141990d8a373d09bda3
Lim Kyunghyuk <yorsild@naver.com>
2022-01-03 19:06:30 +0900

    3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication { // 3
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
